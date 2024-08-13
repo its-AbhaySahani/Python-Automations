975c39ede96f130897ef0832ce44a11255d8f766
Abhay Sahani <abhay.cs.777@gmail.com>
2024-08-13 12:22:28 +0530

    folder delete

--- Folder_Delete_n_days/folder_delete.py
+++ Folder_Delete_n_days/folder_delete.py
@@ -10,6 +10,6 @@ def delete_old_files(root_dir_path, days):
         if os.path.isfile(file_path):
             if(current_time - os.stat(file_path).st_birthtime) > days * 86400:
                 os.remove(file_path)
-
+ 
 if __name__ == '__main__':
     delete_old_files('/Users/username/Downloads/', 7)
