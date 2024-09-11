ac8c57802000d5c504a10532278b3923e0cbaa59
Abhay Sahani <abhay.cs.777@gmail.com>
2024-09-11 12:55:23 +0530

    email

--- EmailAutomation/Email-with-attachment.py
+++ EmailAutomation/Email-with-attachment.py
@@ -1,27 +1,5 @@
 import smtplib
-"""
-This script sends an email with an attachment using the smtplib and email libraries.
-Modules:
-    smtplib: Defines an SMTP client session object that can be used to send mail.
-    email.mime.multipart: Defines the MIMEMultipart class, which represents a multipart MIME message.
-    email.mime.text: Defines the MIMEText class, which represents text MIME objects.
-    email.mime.application: Defines the MIMEApplication class, which represents application MIME objects.
-Configuration:
-    smtp_server (str): The SMTP server address.
-    smtp_port (int): The port to use for the SMTP server.
-    smtp_username (str): The username for the SMTP server.
-    smtp_password (str): The password for the SMTP server.
-    from_email (str): The sender's email address.
-    to_email (str): The recipient's email address.
-    subject (str): The subject of the email.
-    body (str): The body text of the email.
-Usage:
-    This script reads a PDF file named 'report.pdf' from the current directory and attaches it to the email.
-    It then sends the email to the specified recipient using the provided SMTP server credentials.
-Example:
-    To use this script, update the smtp_username, smtp_password, from_email, and to_email variables with
-    appropriate values. Ensure that 'report.pdf' is present in the current directory.
-"""
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
@@ -29,11 +7,11 @@ from email.mime.application import MIMEApplication
 
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
-smtp_username = 'youremail@gmail.com'
-smtp_password = 'yourpassword'
+smtp_username = 'abhay.cs.777@gmail.com'
+smtp_password = 'password'
 
-from_email = 'youremail@gmail.com'
-to_email = 'recipient@example.com'
+from_email = 'abhay.cs.777@gmail.com'
+to_email = 'abhay.12115826@lpu.in'
 subject = 'Email with attachment'
 body = 'Please find attached the report.'
 
