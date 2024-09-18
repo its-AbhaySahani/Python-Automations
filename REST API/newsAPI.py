0429128905317682870e2bb33de58937ee4c662b
Abhay Sahani <abhay.cs.777@gmail.com>
2024-09-18 12:54:01 +0530

    news api

--- REST API/newsAPI.py
+++ REST API/newsAPI.py
@@ -1,27 +1,41 @@
+# importing requests package
 import requests
-def NewsfromBBC():
-    # bbc news api parameter
-    query_params = {
-        "sources": "bbc-news",
-        "apikey": "4dbc17e007ab436fb66416009dfb59a8"
 
+def NewsFromBBC():
+
+    query_params = {
+    "source": "bbc-news",
+    "sortBy": "top",
+    "apiKey": "ef2a5e17463242299c5a9c33c7a5df2d"
     }
-    main_url = " https://newsapi.org/v2/top-headlines"
+    main_url = " https://newsapi.org/v1/articles"
+    
     # fetching data in json format
     res = requests.get(main_url, params=query_params)
     open_bbc_page = res.json()
-   # check if the response contains article or not
-    if open_bbc_page["status"] == "ok":
-        # getting all articles in a string article
-        article = open_bbc_page["articles"]
-        results = []
-        for ar in article:
-            results.append(ar["title"])
-        return results
-    else:
-        return "Error in fetching news"
     
-    # print all trending news
+    # getting all articles in a string article
+    article = open_bbc_page["articles"]
+    
+    # empty list which will
+    # contain all trending news
+    results = []
+    
+    for ar in article:
+        results.append(ar["title"])
+    
     for i in range(len(results)):
+    
+    # printing all trending news
         print(i + 1, results[i])
     
+    #to read the news out loud for us
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.Spvoice")
+    speak.Speak(results)
+
+# Driver Code
+if __name__ == '__main__':
+
+# function call
+    NewsFromBBC()
